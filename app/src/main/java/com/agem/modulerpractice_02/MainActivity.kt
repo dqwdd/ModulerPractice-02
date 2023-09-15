3c36866bd56a99a69a2f96fef8c0c6637645db7a
dqwdd-mega <dqwdd@megastudy.net>
2023-09-15 17:55:16 +0900

    [Test] 필기 추가
    Commit Date: 2023-09-15(금)

--- app/src/main/java/com/agem/modulerpractice_02/MainActivity.kt
+++ app/src/main/java/com/agem/modulerpractice_02/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 모듈간의 상호참조는 안된다.
+        // moduletest 모듈에서 build.gradle.kts 에서 지울게 좀 있다.
+        // 이번엔 view 가 들어가서 남길 건 남겼는데 data 모듈로 쓴다던지 하는 상황에서는 더 지워도 된다.
+        // 사용하는 app 의 build.gradle.kts 에서 dependencies 에서 project 를 추가해야 한다
         val ttt = findViewById<TextView>(R.id.tvTest)
         ttt.setOnClickListener {
             val myIntent = Intent(this, ModuleTestActivity::class.java)
